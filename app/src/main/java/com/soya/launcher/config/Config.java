683b468ea5a4799f4cf529799f06658bf24ef577
41108871 <bsxu1980@gmail.com>
2024-03-04 21:46:20 +0800

    优化Config.java文件

--- app/src/main/java/com/soya/launcher/config/Config.java
+++ app/src/main/java/com/soya/launcher/config/Config.java
@@ -3,8 +3,11 @@ package com.soya.launcher.config;
 import com.soya.launcher.BuildConfig;
 
 public class Config {
-    public static String PACKAGE_NAME = "com.soya.launcher.upgrade";
-    public static String CLASS_NAME = "com.soya.launcher.upgrade.UpgradeActivity";
+    public static String UPDATE_PACKAGE_NAME = "com.soya.launcher.upgrade";
+    public static String UPDATE_CLASS_NAME = "com.soya.launcher.upgrade.UpgradeActivity";
+
+    public static String STORE_PACKAGE_NAME = "com.soya.store";
+    public static String STORE_CLASS_NAME = "com.soya.store.ui.activity.AppDetialActivity";
 
     //修改渠道信息，修改build.gradle
     //0、弘信 1、爱泊优投影仪 2、爱泊优TV 3、玥芯通(TV-X98K)
