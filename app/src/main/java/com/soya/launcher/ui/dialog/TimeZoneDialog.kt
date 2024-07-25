58d06c4e5f3a41d8ff70c3837412c9aabf31da1a
CoderZeng <chienzy@yeah.net>
2024-07-25 20:33:46 +0800

    修改时区排序

--- app/src/main/java/com/soya/launcher/ui/dialog/TimeZoneDialog.kt
+++ app/src/main/java/com/soya/launcher/ui/dialog/TimeZoneDialog.kt
@@ -1,96 +1,108 @@
-package com.soya.launcher.ui.dialog;
+package com.soya.launcher.ui.dialog
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.leanback.widget.VerticalGridView
+import androidx.lifecycle.lifecycleScope
+import com.soya.launcher.R
+import com.soya.launcher.adapter.TimeZoneAdapter
+import com.soya.launcher.bean.SimpleTimeZone
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.Collections
+import java.util.TimeZone
 
-import androidx.leanback.widget.VerticalGridView;
+class TimeZoneDialog : SingleDialogFragment() {
+    private var mContentGrid: VerticalGridView? = null
 
-import com.soya.launcher.R;
-import com.soya.launcher.adapter.TimeZoneAdapter;
-import com.soya.launcher.bean.SimpleTimeZone;
+    private var mTimeZoneAdapter: TimeZoneAdapter? = null
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TimeZone;
+    private var mBlur: ImageView? = null
+    private var mRootView: View? = null
 
-public class TimeZoneDialog extends SingleDialogFragment{
-    public static final String TAG = "TimeZoneDialog";
-    public static TimeZoneDialog newInstance() {
+    private var callback: Callback? = null
 
-        Bundle args = new Bundle();
 
-        TimeZoneDialog fragment = new TimeZoneDialog();
-        fragment.setArguments(args);
-        return fragment;
+    override fun getLayout(): Int {
+        return R.layout.dialog_time_zone
     }
 
-    private VerticalGridView mContentGrid;
+    override fun init(inflater: LayoutInflater, view: View) {
+        super.init(inflater, view)
+        mContentGrid = view.findViewById(R.id.content)
+        mBlur = view.findViewById(R.id.blur)
+        mRootView = view.findViewById(R.id.root)
 
-    private TimeZoneAdapter mTimeZoneAdapter;
-
-    private ImageView mBlur;
-    private View mRootView;
-
-    private Callback callback;
-
-
-    @Override
-    protected int getLayout() {
-        return R.layout.dialog_time_zone;
+        mTimeZoneAdapter = TimeZoneAdapter(activity, inflater, ArrayList())
     }
 
-    @Override
-    protected void init(LayoutInflater inflater, View view) {
-        super.init(inflater, view);
-        mContentGrid = view.findViewById(R.id.content);
-        mBlur = view.findViewById(R.id.blur);
-        mRootView = view.findViewById(R.id.root);
+    override fun initBind(inflater: LayoutInflater, view: View) {
+        super.initBind(inflater, view)
 
-        mTimeZoneAdapter = new TimeZoneAdapter(getActivity(), inflater, new ArrayList<>());
-    }
-
-    @Override
-    protected void initBind(LayoutInflater inflater, View view) {
-        super.initBind(inflater, view);
-
-        TimeZone aDefault = TimeZone.getDefault();
-        String[] ids = TimeZone.getAvailableIDs();
-        List<SimpleTimeZone> list = new ArrayList<>();
-        int select = 0;
-        for (int i = 0; i < ids.length; i++){
-            String id = ids[i];
-            if (id.equals(aDefault.getID())) select = i;
-            TimeZone zone = TimeZone.getTimeZone(id);
-            list.add(new SimpleTimeZone(zone, id, zone.getDisplayName()));
-        }
-        mTimeZoneAdapter.setSelect(aDefault);
-        mTimeZoneAdapter.replace(list);
-        mContentGrid.setAdapter(mTimeZoneAdapter);
-        mContentGrid.setColumnWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
-        mContentGrid.setSelectedPosition(select);
-        blur(mRootView, mBlur);
-
-        mTimeZoneAdapter.setCallback(new TimeZoneAdapter.Callback() {
-            @Override
-            public void onClick(SimpleTimeZone bean) {
-                if (callback != null) callback.onClick(bean);
+        lifecycleScope.launch {
+            withContext(Dispatchers.Main){
+                blur(mRootView, mBlur)
             }
-        });
+            var select = 0
+            val list: MutableList<SimpleTimeZone> = ArrayList()
+            val aDefault = TimeZone.getDefault()
+            withContext(Dispatchers.IO){
+
+                val ids = TimeZone.getAvailableIDs()
+
+
+                for (i in ids.indices) {
+                    val id = ids[i]
+                    if (id == aDefault.id) select = i
+                    val zone = TimeZone.getTimeZone(id)
+                    list.add(SimpleTimeZone(zone, id, zone.displayName))
+                }
+
+                // 排序集合
+                Collections.sort(list, Comparator.comparingInt { o: SimpleTimeZone -> o.zone.rawOffset })
+            }
+            withContext(Dispatchers.Main){
+                mTimeZoneAdapter!!.setSelect(aDefault)
+                mTimeZoneAdapter!!.replace(list)
+                mContentGrid!!.adapter = mTimeZoneAdapter
+                mContentGrid!!.setColumnWidth(ViewGroup.LayoutParams.WRAP_CONTENT)
+                mContentGrid!!.selectedPosition = select
+
+
+                mTimeZoneAdapter!!.setCallback { bean -> if (callback != null) callback!!.onClick(bean) }
+            }
+        }
+
+
+
+
     }
 
-    @Override
-    protected float getDimAmount() {
-        return 0;
+    override fun getDimAmount(): Float {
+        return 0f
     }
 
-    public void setCallback(Callback callback) {
-        this.callback = callback;
+    fun setCallback(callback: Callback?) {
+        this.callback = callback
     }
 
-    public interface Callback{
-        void onClick(SimpleTimeZone bean);
+    interface Callback {
+        fun onClick(bean: SimpleTimeZone?)
+    }
+
+    companion object {
+        const val TAG: String = "TimeZoneDialog"
+        @JvmStatic
+        fun newInstance(): TimeZoneDialog {
+            val args = Bundle()
+
+            val fragment = TimeZoneDialog()
+            fragment.arguments = args
+            return fragment
+        }
     }
 }
