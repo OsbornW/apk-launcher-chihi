ca759bb2e7f624df7147e6d760bfbf41d266fe4b
chenhao <3510619299@qq.com>
2024-07-26 15:42:27 +0800

    修复X98k点击事件

--- app/src/main/java/com/soya/launcher/view/NoDragVerticalGridView.kt
+++ app/src/main/java/com/soya/launcher/view/NoDragVerticalGridView.kt
@@ -11,14 +11,54 @@ class NoDragVerticalGridView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
 ) : VerticalGridView(context, attrs, defStyle) {
 
+    private var startX: Float = 0f
+    private var startY: Float = 0f
+    private val CLICK_THRESHOLD = 10 // 设定一个点击阈值
+
 
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
-        if (event?.action == MotionEvent.ACTION_MOVE) {
+  /*      if (event?.action == MotionEvent.ACTION_MOVE) {
             // 返回 true 以拦截触摸事件，禁止拖拽
             return true
         }
         return super.dispatchTouchEvent(event)
-    }
+    }*/
 
+
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                // 记录初始位置
+                startX = event.x
+                startY = event.y
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                // 可以在这里处理拖动事件
+                return false
+
+            }
+            MotionEvent.ACTION_UP -> {
+                // 计算移动距离
+                val endX = event.x
+                val endY = event.y
+                val distanceX = endX - startX
+                val distanceY = endY - startY
+                val distance = Math.sqrt((distanceX * distanceX + distanceY * distanceY).toDouble())
+
+                if (distance < CLICK_THRESHOLD) {
+                    // 认为是点击事件
+                    //onClick()
+                    performClick()
+                    return true
+                } /*else {
+                    // 认为是拖动事件
+                    onDrag()
+                }*/
+                return false
+            }
+        }
+        return false
+    }
 
 }
