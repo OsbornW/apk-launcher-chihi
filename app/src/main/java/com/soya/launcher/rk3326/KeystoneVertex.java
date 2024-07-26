2cc8a63ac1c517b157f5135d4d64ab5b7165794c
CoderZeng <chienzy@yeah.net>
2024-07-26 15:41:14 +0800

    修复H6梯形校正

--- app/src/main/java/com/soya/launcher/rk3326/KeystoneVertex.java
+++ app/src/main/java/com/soya/launcher/rk3326/KeystoneVertex.java
@@ -31,6 +31,21 @@ public class KeystoneVertex {
             vBottomRight = new Vertex(sBottomRight);
     }
 
+    public void getAllKeystoneVertexForH6() {
+        String sTopLeft = ReflectUtils.getProperty(PROP_KEYSTONE_BOTTOM_LEFT, "0,0");
+        String sTopRight = ReflectUtils.getProperty(PROP_KEYSTONE_BOTTOM_RIGHT, "0,0");
+        String sBottomLeft = ReflectUtils.getProperty(PROP_KEYSTONE_TOP_LEFT, "0,0");
+        String sBottomRight = ReflectUtils.getProperty(PROP_KEYSTONE_TOP_RIGHT, "0,0");
+
+        Log.d("keystone", sTopLeft + " " + sTopRight + " " + sBottomLeft + " " + sBottomRight);
+
+        vTopLeft = new Vertex(sTopLeft);
+        vTopRight = new Vertex(sTopRight);
+        vBottomLeft = new Vertex(sBottomLeft);
+        vBottomRight = new Vertex(sBottomRight);
+    }
+
+
     public void updateAllKeystoneVertex() {
             ReflectUtils.setProperty(PROP_KEYSTONE_TOP_LEFT, vTopLeft.toString());
             ReflectUtils.setProperty(PROP_KEYSTONE_TOP_RIGHT, vTopRight.toString());
@@ -39,6 +54,16 @@ public class KeystoneVertex {
             ReflectUtils.setProperty(PROP_KEYSTONE_UPDATE, "1");
 
     }
+
+    public void updateAllKeystoneVertexForH6() {
+        ReflectUtils.setProperty(PROP_KEYSTONE_BOTTOM_LEFT, vTopLeft.toString());
+        ReflectUtils.setProperty(PROP_KEYSTONE_BOTTOM_RIGHT, vTopRight.toString());
+        ReflectUtils.setProperty(PROP_KEYSTONE_TOP_LEFT, vBottomLeft.toString());
+        ReflectUtils.setProperty(PROP_KEYSTONE_TOP_RIGHT, vBottomRight.toString());
+        ReflectUtils.setProperty(PROP_KEYSTONE_UPDATE, "1");
+
+    }
+
 }
 
 
