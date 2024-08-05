e0ca35740b7032f5fcea5e87b87ee651690fa28d
chenhao <3510619299@qq.com>
2024-08-05 11:26:12 +0800

    修改Release运行时debug模式

--- app/src/hongxin_H27002/java/com/soya/launcher/ext/FlavoursUtils.kt
+++ app/src/hongxin_H27002/java/com/soya/launcher/ext/FlavoursUtils.kt
@@ -17,7 +17,7 @@ import com.soya.launcher.ui.fragment.MainFragment
 import com.soya.launcher.ui.fragment.WelcomeFragment
 
 fun switchFragment() = run {
-/*
+
     BuildConfig.DEBUG.yes {
         sendLiveEventDataDelay(IS_MAIN_CANBACK, false, 1000)
         MainFragment.newInstance()
@@ -30,14 +30,8 @@ fun switchFragment() = run {
         }.otherwise {
             AuthFragment.newInstance()
         }
-    }*/
-    AppCacheBase.isActive.yes {
-        sendLiveEventDataDelay(IS_MAIN_CANBACK, false, 1000)
-        MainFragment.newInstance()
-
-    }.otherwise {
-        AuthFragment.newInstance()
     }
+
 
 }
 
