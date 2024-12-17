d13f15bb150cd1243638662182349782872df6fe
CoderZeng <chienzy@yeah.net>
2024-12-17 17:48:00 +0800

    P50二合一

--- app/src/main/java/com/soya/launcher/product/ProjectorP50.kt
+++ app/src/main/java/com/soya/launcher/product/ProjectorP50.kt
@@ -3,6 +3,8 @@ package com.soya.launcher.product
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import com.shudong.lib_base.ext.appContext
+import com.shudong.lib_base.ext.otherwise
+import com.shudong.lib_base.ext.yes
 import com.soya.launcher.PACKAGE_NAME_FILE_MANAGER_713
 import com.soya.launcher.PACKAGE_NAME_PROJECTOR_MODE_P50
 import com.soya.launcher.PACKAGE_NAME_SCREEN_ZOOM_P50
@@ -12,6 +14,8 @@ import com.soya.launcher.bean.SettingItem
 import com.soya.launcher.bean.TypeItem
 import com.soya.launcher.enums.Types
 import com.soya.launcher.ext.autoResponseText
+import com.soya.launcher.ext.convertGameJson
+import com.soya.launcher.ext.isGame
 import com.soya.launcher.ext.openApp
 import com.soya.launcher.p50.setFunctionCorrectionAndFocus
 import com.soya.launcher.product.base.TVDeviceImpl
@@ -196,5 +200,17 @@ open class ProjectorP50 : TVDeviceImpl {
     }
 
     override fun isShowPageTitle() = false
+
+    override fun addGameItem(): MutableList<TypeItem>? = run {
+        isGame().yes {
+            convertGameJson()
+        }.otherwise { null }
+    }
+
+    override fun isGameRes(): Int = run {
+        isGame().yes {
+            1
+        }.otherwise { 0 }
+    }
 
 }
