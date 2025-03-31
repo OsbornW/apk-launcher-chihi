7b9a40b238ebbf8d41e2b716db4e4b9949dcf170
CoderZeng <chienzy@yeah.net>
2025-03-31 16:59:03 +0800

    优化H5显示页面

--- baselibrary/lib_base/src/main/java/com/shudong/lib_base/BaseWebActivity.kt
+++ baselibrary/lib_base/src/main/java/com/shudong/lib_base/BaseWebActivity.kt
@@ -1,5 +1,6 @@
 package com.shudong.lib_base
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
@@ -18,10 +19,8 @@ import com.just.agentweb.WebViewClient
 import com.shudong.lib_base.base.BaseVMActivity
 import com.shudong.lib_base.base.BaseViewModel
 import com.shudong.lib_base.databinding.ActivityBaseWebBinding
-import com.shudong.lib_base.ext.appContext
 import com.shudong.lib_base.ext.otherwise
 import com.shudong.lib_base.ext.yes
-import com.thumbsupec.lib_base.ext.language.changeLanguage
 import com.thumbsupec.lib_base.web.WebLayout
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -35,16 +34,7 @@ open class BaseWebActivity : BaseVMActivity<ActivityBaseWebBinding, BaseViewMode
     protected var mAgentWeb: AgentWeb? = null
     private lateinit var url: String
 
-    /**
-     *
-     * 解决打开webview导致语言切换失效问题
-     */
-    override fun onCreate(savedInstanceState: Bundle?) {
-        //WebView(this).destroy()
-        //changeLanguage(appContext, Locale.getDefault().language)
-        super.onCreate(savedInstanceState)
-        loadLayout()
-    }
+
 
     override fun initBeforeContent() {
         hookWebView()
@@ -56,16 +46,18 @@ open class BaseWebActivity : BaseVMActivity<ActivityBaseWebBinding, BaseViewMode
 
     override fun initView() {
         url = intent.getStringExtra(INTENT_WEB_URL) ?: getUrl()
-        println("当前的本地语言是：${Locale.getDefault().language}")
         url = when (Locale.getDefault().language) {
             "zh" -> {
                 val country = Locale.getDefault().country
-                println("当前的本地语言国家是：${country}")
                 when (country.lowercase()) {
                     "tw", "hk", "mo" -> "file:///android_asset/privacy_policy_rTW.html"
                     else -> "file:///android_asset/privacy_policy_rCN.html"
                 }
             }
+            "de"->"file:///android_asset/privacy_policy_de.html"
+            "es"->"file:///android_asset/privacy_policy_es.html"
+            "ja"->"file:///android_asset/privacy_policy_jar.html"
+            "pt"->"file:///android_asset/privacy_policy_pt.html"
 
             else -> "file:///android_asset/privacy_policy_en.html"
         }
@@ -75,79 +67,79 @@ open class BaseWebActivity : BaseVMActivity<ActivityBaseWebBinding, BaseViewMode
 
     private fun getUrl() = "https://m.jd.com/"
 
+    @SuppressLint("MissingPermission")
     private fun initWebView() {
         lifecycleScope.launch {
-            withContext(Dispatchers.IO) {
-                (NetworkUtils.isAvailable()).yes {
-                    withContext(Dispatchers.Main) {
-                        mAgentWeb = AgentWeb.with(this@BaseWebActivity)
-                            .setAgentWebParent(
-                                mBind.llContainer,
-                                RelativeLayout.LayoutParams(-1, -1)
-                            )
-                            .useDefaultIndicator()
-                            .setWebChromeClient(mWebChromeClient)
-                            .setWebViewClient(mWebViewClient)
-                            .setMainFrameErrorView(R.layout.layout_net_error, -1)
-                            .setSecurityType(AgentWeb.SecurityType.STRICT_CHECK)
-                            .setWebLayout(WebLayout(this@BaseWebActivity))
-                            .setOpenOtherPageWays(DefaultWebClient.OpenOtherPageWays.ASK) //打开其他应用时，弹窗咨询用户是否前往其他应用
-                            .interceptUnkownUrl() //拦截找不到相关页面的Scheme
-                            .createAgentWeb()
-                            .ready()
-                            .go(url)
-                        mAgentWeb!!.agentWebSettings.webSettings.useWideViewPort =
-                            true //将图片调整到适合webview的大小
-                        mAgentWeb!!.agentWebSettings.webSettings.loadWithOverviewMode =
-                            true // 缩放至屏幕的大小
-                        mAgentWeb!!.agentWebSettings.webSettings.cacheMode =
-                            LOAD_NO_CACHE // 缩放至屏幕的大小
-                        mAgentWeb?.webCreator?.webView?.isFocusable = true
-                        mAgentWeb?.webCreator?.webView?.isFocusableInTouchMode = true
-                        mAgentWeb?.webCreator?.webView?.setBackgroundColor(Color.parseColor("#1A2C3E")) // 设置背景色
+            (NetworkUtils.isConnected()).yes {
+                mAgentWeb = AgentWeb.with(this@BaseWebActivity)
+                    .setAgentWebParent(
+                        mBind.llContainer,
+                        RelativeLayout.LayoutParams(-1, -1)
+                    )
+                    .useDefaultIndicator()
+                    .setWebChromeClient(mWebChromeClient)
+                    .setWebViewClient(mWebViewClient)
+                    .setMainFrameErrorView(R.layout.layout_net_error, -1)
+                    .setSecurityType(AgentWeb.SecurityType.STRICT_CHECK)
+                    .setWebLayout(WebLayout(this@BaseWebActivity))
+                    .setOpenOtherPageWays(DefaultWebClient.OpenOtherPageWays.ASK) //打开其他应用时，弹窗咨询用户是否前往其他应用
+                    .interceptUnkownUrl() //拦截找不到相关页面的Scheme
+                    .createAgentWeb()
+                    .ready()
+                    .go(url)
+                mAgentWeb!!.agentWebSettings.webSettings.useWideViewPort =
+                    true //将图片调整到适合webview的大小
+                mAgentWeb!!.agentWebSettings.webSettings.loadWithOverviewMode =
+                    true // 缩放至屏幕的大小
+                mAgentWeb!!.agentWebSettings.webSettings.cacheMode =
+                    LOAD_NO_CACHE // 缩放至屏幕的大小
+                mAgentWeb?.webCreator?.webView?.isFocusable = true
+                mAgentWeb?.webCreator?.webView?.isFocusableInTouchMode = true
+                mAgentWeb?.webCreator?.webView?.setBackgroundColor(Color.parseColor("#1A2C3E")) // 设置背景色
 
-                    }
-                }.otherwise {
-                    runOnUiThread {
-                        //showErrorToast(com.thumbsupec.lib_res.R.string.web_error_tip.stringValue())
-                        mBind.rlNoData.isVisible = true
-                    }
 
+            }.otherwise {
+                runOnUiThread {
+                    //showErrorToast(com.thumbsupec.lib_res.R.string.web_error_tip.stringValue())
+                    mBind.rlNoData.isVisible = true
                 }
             }
-        }
 
+        }
 
     }
 
-    private val mWebChromeClient: WebChromeClient = object : WebChromeClient() {
-        override fun onReceivedTitle(view: WebView, title: String?) {
-            super.onReceivedTitle(view, title)
 
-        }
+}
+
+private val mWebChromeClient: WebChromeClient = object : WebChromeClient() {
+    override fun onReceivedTitle(view: WebView, title: String?) {
+        super.onReceivedTitle(view, title)
+
+    }
+}
+
+private val mWebViewClient: WebViewClient = object : WebViewClient() {
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        return super.shouldOverrideUrlLoading(view, request)
     }
 
-    private val mWebViewClient: WebViewClient = object : WebViewClient() {
-        override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-            return super.shouldOverrideUrlLoading(view, request)
-        }
-
-        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-            //do you  work
-        }
-
-        override fun onReceivedError(
-            view: WebView?,
-            request: WebResourceRequest?,
-            error: WebResourceError?
-        ) {
-            super.onReceivedError(view, request, error)
-            /*isRepeatExcute().no {
-                showErrorToast(com.thumbsupec.lib_res.R.string.web_error_tip.stringValue())
-                mBind.rlNoData.isVisible = true
-            }*/
-
-        }
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        //do you  work
     }
+
+    override fun onReceivedError(
+        view: WebView?,
+        request: WebResourceRequest?,
+        error: WebResourceError?
+    ) {
+        super.onReceivedError(view, request, error)
+        /*isRepeatExcute().no {
+            showErrorToast(com.thumbsupec.lib_res.R.string.web_error_tip.stringValue())
+            mBind.rlNoData.isVisible = true
+        }*/
+
+    }
+
 
 }
